7cdd06799067cd9e57a74080e5314d09dfde6ddc
Darina Ayurova <comedial@student.21-school.ru>
2023-04-03 22:41:35 +0300

    02-09

---
+++ src/ex09/day00_ex09.sql
@@ -0,0 +1,16 @@
+SELECT 
+    (SELECT person.name 
+    FROM person 
+    WHERE person.id = pv.person_id) 
+    AS person_name , 
+    (SELECT pizzeria.name 
+    FROM pizzeria 
+    WHERE pizzeria.id = pv.pizzeria_id) 
+    AS pizzeria_name 
+FROM 
+    (SELECT person_id, pizzeria_id 
+    FROM person_visits 
+    WHERE visit_date >= '2022-01-07' 
+    AND visit_date <= '2022-01-09') AS pv
+ORDER BY person_name ASC, 
+         pizzeria_name DESC;
